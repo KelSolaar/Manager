ee2c6779bc87e2654d9832b0b41c95b5de92c031
KelSolaar <thomas.mansencal@gmail.com>
2011-08-03 22:57:15 +0100

    Extract "Manager" package related exceptions from "Foundations" package.

---
+++ src/manager/exceptions.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**exceptions.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	This module defines **Manager** package exceptions. 
+
+**Others:**
+
+"""
+
+#***********************************************************************************************
+#***	External imports.
+#***********************************************************************************************
+import logging
+
+#***********************************************************************************************
+#***	Internal imports.
+#***********************************************************************************************
+import foundations.core as core
+import foundations.exceptions
+from manager.globals.constants import Constants
+
+#***********************************************************************************************
+#***	Module attributes.
+#***********************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2011 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+LOGGER = logging.getLogger(Constants.logger)
+
+#***********************************************************************************************
+#***	Module classes and definitions.
+#***********************************************************************************************
+
+class AbstractComponentError(foundations.exceptions.AbstractError):
+	"""
+	This class is the abstract base class for component related exceptions.
+	"""
+
+	pass
+
+class ComponentActivationError(AbstractComponentError):
+	"""
+	This class is used for Component activation exceptions.
+	"""
+
+	pass
+
+class ComponentDeactivationError(AbstractComponentError):
+	"""
+	This class is used for Component deactivation exceptions.
+	"""
+
+	pass
+
+class ComponentReloadError(AbstractComponentError):
+	"""
+	This class is used for Component reload exceptions.
+	"""
+
+	pass
---
+++ src/tests/testsManager/testsExceptions.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**testsExceptions.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	Exceptions tests Module.
+
+**Others:**
+
+"""
+
+#***********************************************************************************************
+#***	External imports.
+#***********************************************************************************************
+import inspect
+import unittest
+
+#***********************************************************************************************
+#***	Internal imports.
+#***********************************************************************************************
+import manager.exceptions
+
+#***********************************************************************************************
+#***	Module attributes.
+#***********************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2011 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+EXCEPTIONS = []
+for attribute in dir(manager.exceptions):
+	object = getattr(manager.exceptions, attribute)
+	if not inspect.isclass(object):
+		continue
+	if issubclass(object, Exception):
+			EXCEPTIONS.append(object)
+
+#***********************************************************************************************
+#***	Module classes and definitions.
+#***********************************************************************************************
+class ExceptionsTestCase(unittest.TestCase):
+	"""
+	This class is the **ExceptionsTestCase** class.
+	"""
+
+	def testRequiredAttributes(self):
+		"""
+		This method tests presence of required attributes.
+		"""
+
+		requiredAttributes = ("value",)
+		for exception in EXCEPTIONS:
+			exceptionInstance = exception(None)
+			for attribute in requiredAttributes:
+				self.assertIn(attribute, dir(exceptionInstance))
+
+	def test__str__(self):
+		"""
+		This method tests **Exceptions** class **__str__** method.
+		"""
+
+		for exception in EXCEPTIONS:
+			exceptionInstance = exception("{0} Exception raised!".format(exception.__class__))
+			self.assertIsInstance(exceptionInstance.__str__(), str)
+			exceptionInstance = exception([exception.__class__, "Exception raised!"])
+			self.assertIsInstance(exceptionInstance.__str__(), str)
+			exceptionInstance = exception(0)
+			self.assertIsInstance(exceptionInstance.__str__(), str)
+
+if __name__ == "__main__":
+	unittest.main()
+
