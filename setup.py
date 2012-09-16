f1af230c92b79479dfdba76a4a9ec861f932ecfd
Thomas Mansencal <thomas.mansencal@gmail.com>
2012-09-16 11:23:38 +0200

    Update "setup.py" file.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-import manager.globals.constants
-
+import re
 from setuptools import setup
 from setuptools import find_packages
+
+import manager.globals.constants
 
 def getLongDescription():
 	"""
@@ -10,14 +11,17 @@ def getLongDescription():
 	:return: Package long description. ( String )
 	"""
 
-	description = str()
+	description = []
 	with open("README.rst") as file:
 		for line in file:
-			if ".. code:: python" in line:
+			if ".. code:: python" in line and len(description) >= 2:
+				blockLine = description[-2]
+				if re.search(r":$", blockLine) and not re.search(r"::$", blockLine):
+					description[-2] = "::".join(blockLine.rsplit(":", 1))
 				continue
 
-			description += line
-	return description
+			description.append(line)
+	return str().join(description)
 
 setup(name=manager.globals.constants.Constants.applicationName,
 	version=manager.globals.constants.Constants.releaseVersion,
